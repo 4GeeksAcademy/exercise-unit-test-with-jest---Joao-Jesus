9cba7cfe6c6d4eb358542ade17ed1a2d694cfbc1
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-02-29 16:54:05 +0000

    4 tests passed

--- test.js
+++ test.js
@@ -9,19 +9,19 @@ test('adds 14 + 9 to equal 23', () => {
 test("One euro should be 1.07 dollars", () => {
 const dollars = fromEuroToDollar(3.5);
 const expected = 3.5 * 1.07; 
-     expect(dollars).toBe(expected); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+     expect(dollars).toBeCloseTo(expected); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
 
-test("One dollar should convert to yen", () => {
+test("Ten dollar's should convert to yen", () => {
     const dollars = fromDollarToYen(10);
     const expectedDollarToEur = 10 / 1.07;
-    const expectedDollarsToYen = expectedDollarToEur * 1.56
-    expect(dollars).toBeCloseTo(expectedDollarsToYen);
+    const expectedDollarsToYen = expectedDollarToEur * 156.5
+    expect(dollars).toBeCloseTo(expectedDollarsToYen)
 }) 
 
-test("One yen should convert to Pound", () => {
+test("Ten yen should convert to Pound", () => {
     const yen = fromYenToPound(10);
-    const expectedYenToEur = 10 / 1.56;
+    const expectedYenToEur = 10 / 156.5;
     const expectedYenToPound = expectedYenToEur * 0.87
-    expect(yen).toBeCloseTo(expectedYenToPound);
+    expect(yen).toBeCloseTo(expectedYenToPound)
 })
